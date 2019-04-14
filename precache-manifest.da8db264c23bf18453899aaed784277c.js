095524c44ad216ee10427e9bc289dfe953884541
Elliot Burin <eburin2@gmail.com>
2019-04-14 14:41:51 -0700

    Updates

---
+++ precache-manifest.da8db264c23bf18453899aaed784277c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff09db3b5a28b59e7605",
+    "url": "/React-Movie-Database-API/static/css/main.ef8e3b5c.chunk.css"
+  },
+  {
+    "revision": "ff09db3b5a28b59e7605",
+    "url": "/React-Movie-Database-API/static/js/main.4fa9cf2f.chunk.js"
+  },
+  {
+    "revision": "8ea0a40b00ab7ac7a3e9",
+    "url": "/React-Movie-Database-API/static/js/runtime~main.dad866d4.js"
+  },
+  {
+    "revision": "fb112ef1252de13b2f2a",
+    "url": "/React-Movie-Database-API/static/css/2.f20066a1.chunk.css"
+  },
+  {
+    "revision": "fb112ef1252de13b2f2a",
+    "url": "/React-Movie-Database-API/static/js/2.625f3db9.chunk.js"
+  },
+  {
+    "revision": "ce8250d42acf52d33967adc3d0d45210",
+    "url": "/React-Movie-Database-API/index.html"
+  }
+];
