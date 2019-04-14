da87fb74168426ab23beb7bcc1bc18664aefee99
Elliot Burin <eburin2@gmail.com>
2019-04-14 15:58:10 -0700

    clean up code

--- src/App.js
+++ src/App.js
@@ -10,22 +10,7 @@ class App extends Component {
     super(props);
     this.state = {term: ''};
     this.searchHandler = this.searchHandler.bind(this);
-    // console.log('yo');
-  
-    // const movies = [
-    //   {id: 0, cover: 'https://images-na.ssl-images-amazon.com/images/I/514K35AM3KL._SY445_.jpg', title: 'Repoman', year: 1984},
-    //   {id: 1, cover: 'https://m.media-amazon.com/images/M/MV5BMjI1MDk4NjE0OV5BMl5BanBnXkFtZTcwMjExODM5NA@@._V1_UY1200_CR84,0,630,1200_AL_.jpg', title: 'Tough Guys Don\'t Dance', year: 1987}
-    // ];
-
     
-    // let movieRows = [];
-    // movies.forEach((movie) => {
-    //   console.log(movie.id);
-    //   const movieRow = <MovieRow movie={movie} />
-    //   movieRows.push(<div key={movie.id}> {movieRow}</div>)
-    // })
-    // this.state = {rows: movieRows}
-  
   this.performSearch();
 }
 
@@ -68,7 +53,7 @@ searchHandler(event) {
       <div>
         <header class="header">
             <ul id="nav-items">
-                <li><img width="80" src="/green-image.png" alt="logo"/></li>
+                <li><img width="80" src="https://cdn0.iconfinder.com/data/icons/electronics-and-devices-5/24/204-512.png" alt="logo"/></li>
                 <li id="title"><h1>MoviesDB Search</h1></li>
             </ul>
         </header>
